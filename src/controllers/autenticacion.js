63f2ba73c3be655b8075e2325e7b9c1d071705df
dasmorphy <93955043+dasmorphy@users.noreply.github.com>
2022-02-27 17:04:51 -0500

    Agregando el controlador usuarios

---
+++ src/controllers/autenticacion.js
@@ -0,0 +1,29 @@
+import { type } from "express/lib/response";
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+        username: {
+            type: String,
+            unique: true
+        },
+        email: {
+            type: String,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        roles: [{
+            ref: "Role",
+            type: Schema.Types.ObjectId
+        }],
+    }, {
+        timestamps: true, //añade hora y fecha de actualizacion o registro
+        versionKey: false //oculta el tipo de version que se uso 
+    }
+
+
+)
+
+export default userSchema;
