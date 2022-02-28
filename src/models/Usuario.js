7f7b9fd25b167047eeb3960321e8380b8d7bc46d
dasmorphy <93955043+dasmorphy@users.noreply.github.com>
2022-02-27 23:01:19 -0500

    Actualizacion del modelo Usuario

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,5 +1,6 @@
-import { type } from "express/lib/response";
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
+
 
 const userSchema = new Schema({
         username: {
@@ -23,7 +24,18 @@ const userSchema = new Schema({
         versionKey: false //oculta el tipo de version que se uso 
     }
 
+);
+userSchema.statics.encryptPassword = async(password) => {
+    const salt = await bcrypt.genSalt(10) //10veces se aplicara el algoritmo
+    return await bcrypt.hash(password, salt) //cifrando contrasena
+}
 
-)
+//Comparar la contraseña cifrada con la que el usuario escribira en el input para que coincidan
+//si coinciden true y sino false
+userSchema.statics.comparePassword = async(password, receivedPassword) => {
+    //password es la contrasena guardada que esta cifrada y receivedPassword es la que 
+    //el usuario esta escribiendo para ser comparada con la que esta guardada y que coincidan 
+    return await bcrypt.compare(password, receivedPassword)
+}
 
 export default userSchema;
